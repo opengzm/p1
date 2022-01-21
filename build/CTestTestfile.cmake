f33a751a956ad469298d29f310d7750c03a22228
opengzm <stone.hehe@163.com>
2022-01-21 16:12:46 +0800

    7 del build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/86186/source/vscode/p1
-# Build directory: C:/Users/86186/source/vscode/p1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
